8de8d6f472e97bdc6ca1c83217b14b946eb922c9
Hubangyan <bangyan.hu@outlook.com>
2025-11-06 18:07:26 +0800

    '123'

---
+++ stack/SqStack.c
@@ -0,0 +1,24 @@
+#define ture 1
+#define false 2
+typedef int flag;
+//顺序栈
+typedef struct SqStack{
+	int data[20];
+	int top;//栈顶指针
+}SqStack;
+
+SqStack InitStack(){
+	
+}
+
+flag StackEmpty(SqStack* p){
+	
+}
+
+flag push(SqStack* p,int element){
+	
+}
+int pop(SqStack* p){
+	
+}
+
