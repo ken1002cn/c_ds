9544481f11c9e192fd6dd24fb3988fe5443ecd34
Hubangyan <bangyan.hu@outlook.com>
2025-11-07 18:17:29 +0800

    '123'

--- stack/SqStack.c
+++ stack/SqStack.c
@@ -1,24 +1,37 @@
+#include<stdio.h>
 #define ture 1
-#define false 2
+#define false -1
+#define MAXSIZE 20
 typedef int flag;
 //顺序栈
 typedef struct SqStack{
-	int data[20];
+	int data[MAXSIZE];
 	int top;//栈顶指针
 }SqStack;
 
-SqStack InitStack(){
-	
+SqStack InitStack(SqStack* p){
+	p->top = -1;
 }
 
 flag StackEmpty(SqStack* p){
-	
+	return p->top == -1;
 }
 
 flag push(SqStack* p,int element){
-	
+	if(p->top == MAXSIZE-1){
+		printf("栈满");
+		return false;
+	}
+	p->data[++p->top] = element;
+	return ture;
 }
-int pop(SqStack* p){
-	
+flag pop(SqStack* p,int* e){
+	if(p->top == -1){
+		printf("栈空");
+		return false;
+	}
+	*e = p->data[p->top];
+	p->top--;
+	return ture;
 }
 
